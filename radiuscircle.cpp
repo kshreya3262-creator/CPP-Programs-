194b0d885e09d87adb27ca152e073198bb3859ae
kshreya3262-creator <kshreya3262@gmail.com>
2026-08-18 15:31:10 +0530

    Create radiuscircle.cpp

---
+++ radiuscircle.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+const float pi=3.14;
+float r,a;
+cout<<"Enter the value of radius r:";
+cin>>r;
+a=pi*r*r;
+cout<<"The area of circle is"<<a;
+return 0;
+}
