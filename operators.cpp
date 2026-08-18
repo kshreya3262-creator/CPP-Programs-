10ac730af81bf694b99d6c69e0e8c61c83f9e919
kshreya3262-creator <kshreya3262@gmail.com>
2026-08-18 15:30:30 +0530

    Create operators.cpp

---
+++ operators.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b,c;
+cout<<"Enter the first number a:";
+cin>>a;
+cout<<"Enter the second number b:";
+cin>>b;
+c=a+b;
+cout<<"The addition of two numbers is:"<<c;
+c=a-b;
+cout<<"The subtraction of two numbers is:"<<c;
+c=a*b;
+cout<<"The multiplication of two numbers is:"<<c;
+c=a%b;
+cout<<"The division of two numbers is:"<<c;
+return 0;
+}
