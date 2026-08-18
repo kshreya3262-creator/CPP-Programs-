a5c9e7766c450df9d5eaa944a8195ecbd90c0cb0
kshreya3262-creator <kshreya3262@gmail.com>
2026-08-18 15:23:35 +0530

    Create add.cpp

---
+++ add.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b,c;
+cout<<"Enter the first number a:";
+cin>>a;
+cout<<"Enter the second number b:";
+cin>>b;
+c=a+b;
+cout<<"The sum of given numbers is"<<c;
+return 0;
+}
