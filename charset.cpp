e50388491fac68692249495f734e6c6862df9a2b
kshreya3262-creator <kshreya3262@gmail.com>
2026-08-18 15:29:09 +0530

    Create charset.cpp

---
+++ charset.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+char letter,digit,special;
+cout<<"Enter a letter";
+cin>>letter;
+cout<<"you entered a letter:"<<letter<<endl;
+cout<<"\n Enter a digit";
+cin>>digit;
+cout<<"You entered a digit:"<<digit<<endl;
+cout<<"\n Enter a special character";
+cin>>special;
+cout<<"You entered a special character:"<<special<<endl;
+return 0;
+}
